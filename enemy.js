3a47c9ee20f0ef2d3a3e76788fee0c9b50042809
saxenamanas <saxenamanas0@gmail.com>
2020-04-18 20:25:57 +0530

    Added collision when bullets hit target.

--- enemy.js
+++ enemy.js
@@ -2,7 +2,7 @@ class Enemy{
     constructor(y){
         this.x = 1066;
         this.y = y;
-        this.w = 50;
+        this.w = 80;
         this.h = 40;
     }
 
@@ -10,10 +10,30 @@ class Enemy{
         image(ufo,this.x,this.y,this.w,this.h);
     }
 
-    move(){
-        if(this.x>=0){
+    move(i){
+        if(this.x>0){
             this.x = this.x - 5;
         }
-      enemies.splice(1,i);
+         else{
+             enemies.splice(i,1);
+         }
     }
+    
+    
+
+     checkCollision(j){
+         try{
+            print("Trying to check");
+         for(let i = 0;i<shots.length;i++){
+             if(checkit(this.x,this.y,this.w,this.h,shots[i].x,shots[i].y,shots[i].w,shots[i].h)==true){
+                 print('target hit');
+                 shots.splice(i,1);
+                 enemies.splice(j,1);
+             }
+         }
+         }catch(e){
+             
+         }
+         
+     }
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let shipX = 1;
 let shipY = 1;
-let shipW = 60;
-let shipH = 40;
+let shipW = 80;
+let shipH = 50;
 let bg;
 var shots = [];
 let bullet;
@@ -30,26 +30,37 @@ function keyPressed(){
 function draw() {
     background(bg);
     image(ship,shipX,shipY,shipW,shipH);
-    if(keyIsDown(DOWN_ARROW))
-      {
-        if(shipY+shipH+5<=600)
-          shipY += 5;
-      }
-    if(keyIsDown(UP_ARROW)){
-      if(shipY-5>=0)
-        shipY -=5;
-    }
-    for(i = 0;i<shots.length;i++){
-      shots[i].show();
-      shots[i].move();
-    }
-    if(random()<=0.08){
+    //print('hi');
+    if(random()<0.01){
       enemies.push(new Enemy(random(600)));
     }
     for(i=0;i<enemies.length;i++){
       enemies[i].show();
       enemies[i].move();
+      enemies[i].checkCollision(i);
     }
-  print(enemies.length);
+
+    for(i = 0;i<shots.length;i++){
+      shots[i].show();
+      shots[i].move(i);
+    }
+    if(keyIsDown(DOWN_ARROW))
+    {
+      if(shipY+shipH+5<=600)
+        shipY += 5;
+    }
+    if(keyIsDown(UP_ARROW)){
+      if(shipY-5>=0)
+        shipY -=5;
+    }
 }
   
+function checkit(x, y, w, h, x2, y2, w2, h2){
+  if (x + w >= x2 &&    // r1 right edge past r2 left
+x <= x2 + w2 &&    // r1 left edge past r2 right
+y + h >= y2 &&    // r1 top edge past r2 bottom
+y <= y2 + h2) {    // r1 bottom edge past r2 top
+  return true;
+}
+return false;
+}
